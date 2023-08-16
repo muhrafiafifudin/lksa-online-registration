839f3f8e065ebf342729c63a44a0c7ede3623d92
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2023-08-16 14:25:46 +0700

    Update asset script & style

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,24 +1,24 @@
 <!-- Font Awesome-->
-<link rel="stylesheet" type="text/css" href="../assets/css/fontawesome.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/fontawesome.css') }}">
 <!-- ico-font-->
-<link rel="stylesheet" type="text/css" href="../assets/css/icofont.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/icofont.css') }}">
 <!-- Themify icon-->
-<link rel="stylesheet" type="text/css" href="../assets/css/themify.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/themify.css') }}">
 <!-- Flag icon-->
-<link rel="stylesheet" type="text/css" href="../assets/css/flag-icon.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/flag-icon.css') }}">
 <!-- Feather icon-->
-<link rel="stylesheet" type="text/css" href="../assets/css/feather-icon.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/feather-icon.css') }}">
 <!-- Plugins css start-->
-<link rel="stylesheet" type="text/css" href="../assets/css/animate.css">
-<link rel="stylesheet" type="text/css" href="../assets/css/chartist.css">
-<link rel="stylesheet" type="text/css" href="../assets/css/date-picker.css">
-<link rel="stylesheet" type="text/css" href="../assets/css/prism.css">
-<link rel="stylesheet" type="text/css" href="../assets/css/vector-map.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/animate.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/chartist.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/date-picker.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/prism.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vector-map.css') }}">
 <!-- Plugins css Ends-->
 <!-- Bootstrap css-->
-<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.css') }}">
 <!-- App css-->
-<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-<link id="color" rel="stylesheet" href="../assets/css/color-1.css" media="screen">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
+<link id="color" rel="stylesheet" href="{{ asset('assets/css/color-1.css') }}" media="screen">
 <!-- Responsive css-->
-<link rel="stylesheet" type="text/css" href="../assets/css/responsive.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/responsive.css') }}">
